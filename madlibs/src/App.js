4d30668a55465368f0d9a6eef65b6c27322c1810
david <davidmcconkey@outlook.com>
2023-07-28 11:45:39 -0400

    Part 3: Madlibs

---
+++ madlibs/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import React from "react";
+import Madlib from "./Madlib";
+function App() {
+  return (
+    <div>
+      <Madlib />
+    </div>
+  );
+}
+
+export default App;
---
+++ madlibs/src/Madlib.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import MadlibForm from "./MadlibForms";
+import Story from "./Story";
+
+const Madlib = () => {
+  const [storyMade, setStoryMade] = useState(false);
+  const [story, setStory] = useState();
+
+  const createStory = (formData) => {
+    const { noun, adjective, color, noun2 } = formData;
+
+    setStory(
+      `the very ${noun} had a big ${adjective} that showed a bright${color} and never ${noun2}`
+    );
+
+    setStoryMade((storyMade) => !storyMade);
+  };
+
+  const resetStory = () => {
+    setStoryMade((storyMade) => !storyMade);
+    setStory("");
+  };
+
+  return (
+    <>
+      <h1>Madlibs!</h1>
+
+      {storyMade === false ? (
+        <MadlibForm createStory={createStory} />
+      ) : (
+        <Story story={story} reset={resetStory} />
+      )}
+    </>
+  );
+};
+
+export default Madlib;
